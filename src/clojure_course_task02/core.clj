ed2ecf4edb89379f61388aca5600174a8ab0bf1e
Alexey Romanov <alexey.v.romanov@gmail.com>
2013-03-27 21:21:49 +0400

    Initial version

--- src/clojure_course_task02/core.clj
+++ src/clojure_course_task02/core.clj
@@ -1,10 +1,16 @@
 (ns clojure-course-task02.core
   (:gen-class))
 
-
 (defn find-files [file-name path]
-  "TODO: Implement searching for a file using his name as a regexp."
-  nil)
+  "Implement searching for a file using his name as a regexp."
+  (let [root (clojure.java.io/as-file path)
+        f #(find-files file-name %)
+        file-name-re (re-pattern file-name)]
+    (if 
+      (.isDirectory root)
+        (flatten (pmap f (.listFiles root)))
+        (let [name (.getName root)]
+          (if (re-matches file-name-re name) [name] [])))))
 
 (defn usage []
   (println "Usage: $ run.sh file_name path"))
